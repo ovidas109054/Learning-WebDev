51e0ae6253279da5a2e1a608ff56779514edbfa5
ovidas109054 <ovidas109054@gmail.com>
2026-07-17 11:39:25 +0600

    JavaScript learnign

---
+++ hello.js
@@ -0,0 +1,13 @@
+// console.log("Hello Java Script!");
+// let a = 25;
+// let b = 32;
+// let sum = a + b;
+// console.log("The sum of and b is : "+ sum);
+// name = "Ovi Das"
+// console.log(name);
+
+a = 5;
+b = 4;
+sum = a + b;
+console.log(sum);
+
